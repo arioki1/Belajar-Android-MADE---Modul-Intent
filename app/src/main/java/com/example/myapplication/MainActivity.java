1ffb42bbda9d0086f1d8febff4f264137ab05ab7
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-08 08:23:09 +0700

    Intent - Latihan : Intent dengan ResultActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnMoveActivity;
     Button btnMoveWithDataActtivity;
     Button btnMoveWithObject;
+    Button btnDialPhone;
+    Button btnMoveFromResult;
+    TextView tvResult;
+    private int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnMoveWithObject = findViewById(R.id.btn_move_activity_object);
         btnMoveWithObject.setOnClickListener(this);
+
+        btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+
+        btnMoveFromResult = findViewById(R.id.btn_move_with_result);
+        btnMoveFromResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
+
     }
 
     @Override
@@ -50,6 +70,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
 
                 startActivity(moveWithObjectIntent);
+                break;
+            case R.id.btn_dial_number:
+                String phoneNumber = "082329949292";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    //request permission from user if the app hasn't got the required permission
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
+                            REQUEST_CODE);
+                    return;
+                }else {     //have got permission
+                    try{
+                        startActivity(dialPhoneIntent);  //call activity and make phone call
+                    }
+                    catch (android.content.ActivityNotFoundException ex){
+                        Toast.makeText(getApplicationContext(),"yourActivity is not founded", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            case R.id.btn_move_with_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent,REQUEST_CODE);
+                break;
+
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == MoveForResultActivity.RESULT_CODE){
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE,0);
+                tvResult.setText(String.format("hasil: %s",selectedValue));
+            }
         }
     }
 }
